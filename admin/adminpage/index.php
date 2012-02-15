d98cadf64a02ee8abc4779b9bc5133269e91f038
ser0n <andre.lilja@gmail.com>
2012-02-15 23:06:54 +0100

    More magic done to graphical javascript

--- admin/adminpage/index.php
+++ admin/adminpage/index.php
@@ -10,14 +10,25 @@
 		<div id="background" class="background">
 			<?php include("navigation.html") ?>
 			<div id="content" class="content">
-				<div id="maindrop" class="maindrop" ondrop="drop(event)" ondragover="allowDrop(event)">
-				</div>
+				<form name="templateform" action="" method="GET">
+					<div id="namebox" class="namebox">
+						Name:
+						<input type="text" name="name" class="nameTXB"/>
+						<input type="button" name="savetemplatebutton" value="Save template" class="savetemplatebutton" onclick="saveTemplate(this.form)">
+					</div>
+					<div id="notebox" class="notebox">
+						Note:
+						<textarea name="note" class="noteTXB"></textarea>
+					</div>
+				</form>
+				<div id="maindrop" class="maindrop" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
 				<div id="subdrop" class="subdrop" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
 				<div id="contentlist" class="contentlist" ondrop="drop(event)" ondragover="allowDrop(event)">
+					<input type="button" name="button" value="save" class="spawnbutton" onclick="createContTEST()">
 					<div id="contentspawner" class="contentspawner">
 						<form name="itemform" action="" method="GET">
 							<input type="text" name="input" class="contentTXB"/>
-							<INPUT type="button" name="button" value="save" class="savebutton" onClick="createCont(this.form)">
+							<input type="button" name="button" value="save" class="savebutton" onclick="createCont(this.form)">
 						</form>
 					</div>
 				</div>
