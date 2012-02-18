199bdf167228c48f990d022b1a65ea4c7619dd95
ser0n <andre.lilja@gmail.com>
2012-02-18 16:16:06 +0100

    fixed broken link

--- admin/adminpage/admintemplate.php
+++ admin/adminpage/admintemplate.php
@@ -3,7 +3,7 @@
 <html>
 	<head>
 		<title>admintemplate</title>
-		<link rel="stylesheet" type="text/css" href="../css/admin.css"/>
+		<link rel="stylesheet" type="text/css" href="admin.css"/>
 		<script type="text/javascript" src="jquery-1.7.1.js"></script>
 		<script type="text/javascript" src="template.js"></script>
 		<meta charset="UTF-8"/>
@@ -13,7 +13,7 @@
 			<?php include("navigation.html") ?>
 			<div id="content" class="content">
 				<div id="templatelist" class="templatelist">
-					<?php include('listdir.php') ?>
+					<?php include("listdir.php") ?>
 				</div>
 				<div id="bottombar" class="bottombar">
 					<div id="new template" class="menuitem">New template<a href="template.php?p=1"><span class="linkspan"></span></a></div>
