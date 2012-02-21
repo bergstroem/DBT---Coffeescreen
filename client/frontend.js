87a23fdba3aaee3ed0fbaf84015e1e9955775259
Erik Hedvall <erikwhedvall@gmail.com>
2012-02-21 13:33:56 +0100

    Content scrolling is working now. I think the timing is ok

--- client/frontend.js
+++ client/frontend.js
@@ -1,31 +1,83 @@
 var retries = 0;
 var currentInformation = null;
 var mainContentCounter = 0;
+var mainContentProgressTimeout = null;
+
+var running = false;
 
 window.onload = init;
 
 function init() {
 	connectToServer();
-	switchMainInformation();
 }
 
 function switchMainInformation() {
-	var displayTime = 1000;
-	if(currentInformation != null) {
-		var title = currentInformation.maincontent.posts[mainContentCounter].title;
-		var date = currentInformation.maincontent.posts[mainContentCounter].date;
-		var content = currentInformation.maincontent.posts[mainContentCounter].content;
-		document.getElementById("mainContent").innerHTML =
-				"<h1>" + title + "</h1>" +
-				"<p>" + date + "</p>" +
-				"<p>" + content + "</p>";
-		if(++mainContentCounter >= currentInformation.maincontent.posts.length) {
-			mainContentCounter = 0;
-			//TODO: request new information
+	var title = currentInformation.maincontent.posts[mainContentCounter].title;
+	var date = currentInformation.maincontent.posts[mainContentCounter].date;
+	var content = currentInformation.maincontent.posts[mainContentCounter].content;
+	document.getElementById("mainContent").innerHTML =
+			"<h1>" + title + "</h1>" +
+			"<p>" + date + "</p>" +
+			"<p>" + content + "</p>";
+			
+	var images = document.getElementById("mainContent").getElementsByTagName("img");
+	var urls = new Array();
+	if(images.length > 0){
+		for(var i = 0; i < images.length; i++){
+			urls.push(images[i].src);
 		}
 	}
+	preloadimages(urls).done(mainPostLoaded);
 	
+	if(++mainContentCounter >= currentInformation.maincontent.posts.length) {
+		mainContentCounter = 0;
+		//TODO: request new information
+	}
+}
+
+function mainPostLoaded() {
+	var displayTime = 100;
+	displayTime *= document.getElementById("mainContent").offsetHeight;
+	console.debug("Will display for " + (displayTime/1000) + "s");
 	setTimeout(switchMainInformation, displayTime);
+	document.getElementById("pageWrapper").scrollTop = 0;
+	clearTimeout(mainContentProgressTimeout);
+	scrollMainContent(Math.ceil(displayTime/document.getElementById("pageWrapper").scrollHeight));
+}
+
+function scrollMainContent(stepTime) {
+	document.getElementById("pageWrapper").scrollTop += 1;
+	if(document.getElementById("pageWrapper").scrollTop <
+			document.getElementById("pageWrapper").scrollHeight) {
+		mainContentProgressTimeout = setTimeout(scrollMainContent, stepTime, stepTime);
+	}
+}
+
+function preloadimages(arr){
+    var newimages=[], loadedimages=0
+    var postaction=function(){}
+    var arr=(typeof arr!="object")? [arr] : arr
+    function imageloadpost(){
+        loadedimages++
+        if (loadedimages==arr.length){
+            postaction()
+        }
+    }
+    for (var i=0; i<arr.length; i++){
+        newimages[i]=new Image()
+        newimages[i].src=arr[i]
+        newimages[i].onload=function(){
+            imageloadpost()
+        }
+        newimages[i].onerror=function(){
+            imageloadpost()
+        }
+    }
+    return { //return blank object with done() method
+        done:function(f){
+            postaction=f || postaction
+        }
+    }
 }
 
 function connectToServer () {
@@ -67,12 +119,14 @@ function connectToServer () {
         try {
             var json = JSON.parse(message.data);
 			currentInformation = json;
+			if(!running){
+				switchMainInformation();
+				running = true;
+			}
         } catch (e) {
             console.log('This doesn\'t look like a valid JSON: ', message.data);
             return;
         }
-        // handle incoming message
-		console.log(message.data);
     };
 };
 
