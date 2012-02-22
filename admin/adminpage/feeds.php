9ca0ca74a76af3031bf1f90ccadb7c8ef83a05e2
ser0n <andre.lilja@gmail.com>
2012-02-22 01:21:35 +0100

    Started on issue #15 / added feeds page

--- admin/adminpage/feeds.php
+++ admin/adminpage/feeds.php
@@ -3,9 +3,10 @@
 		<title>Feeds</title>
 		<link rel="stylesheet" type="text/css" href="css/admin.css"/>
 		<script type="text/javascript" src="js/jquery-1.7.1.js"></script>
+		<script type="text/javascript" src="js/feeds.js"></script>
 		<meta charset="UTF-8"/>
 	</head>
-	<body>
+	<body onload="powerUsage()">
 		<div id="background" class="background">
 			<?php include("navigation.html") ?>
 			<div id="feedcontent" class="content">
@@ -13,6 +14,7 @@
 					Name:
 					<input type="button" id="newFeedButton" class="newFeedButton" value="New Feed"/>
 				</div>
+				<div id="temperature" class="templateitem"><text id="temp"></text></div>
 			</div>
 		</div>
 	</body>
