26be7410fa0b3138b1ea654e0090f3d8f49d8fbb
Erik Hedvall <erikwhedvall@gmail.com>
2012-04-11 12:09:48 +0200

    Wrote some comments in frontend.js

--- client/frontend.js
+++ client/frontend.js
@@ -12,8 +12,11 @@ function init() {
 	connectToServer();
 }
 
+//Switch to next view
 function switchMainInformation() {
 	console.debug("Switching...");
+	//Extract article info
+	//TODO: Change it to the new system!
 	var title = currentInformation.maincontent.posts[mainContentCounter].title;
 	var date = currentInformation.maincontent.posts[mainContentCounter].date;
 	var content = currentInformation.maincontent.posts[mainContentCounter].content;
@@ -21,7 +24,8 @@ function switchMainInformation() {
 			"<h1>" + title + "</h1>" +
 			"<p>" + date + "</p>" +
 			"<p>" + content + "</p>";
-			
+	
+	//Preload images
 	var images = document.getElementById("mainContent").getElementsByTagName("img");
 	var urls = new Array();
 	if(images.length > 0){
@@ -37,22 +41,28 @@ function switchMainInformation() {
 		mainPostLoaded();
 	}
 	
+	//Ask for new info when the cycle is done
 	if(++mainContentCounter >= currentInformation.maincontent.posts.length) {
 		mainContentCounter = 0;
 		//TODO: request new information
 	}
 }
 
+//What to do when all the images are loaded in a view
 function mainPostLoaded() {
+	//Calculate time to display the view
 	var displayTime = 100;
 	displayTime *= document.getElementById("mainContent").offsetHeight;
 	console.debug("Will display for " + (displayTime/1000) + "s");
+
+	//Setup display and scroll timers
 	mainContentSwitchingTimeout = setTimeout(switchMainInformation, displayTime);
 	document.getElementById("pageWrapper").scrollTop = 0;
 	clearTimeout(mainContentProgressTimeout);
 	scrollMainContent(Math.ceil(displayTime/document.getElementById("pageWrapper").scrollHeight));
 }
 
+//One scrolling jump
 function scrollMainContent(stepTime) {
 	document.getElementById("pageWrapper").scrollTop += 1;
 	if(document.getElementById("pageWrapper").scrollTop <
@@ -61,6 +71,7 @@ function scrollMainContent(stepTime) {
 	}
 }
 
+//Image preloading magic!
 function preloadimages(arr){
     var newimages=[], loadedimages=0
     var postaction=function(){}
@@ -88,6 +99,7 @@ function preloadimages(arr){
     }
 }
 
+//Do connecting stuff
 function connectToServer () {
     // if user is running mozilla then use it's built-in WebSocket
     window.WebSocket = window.WebSocket || window.MozWebSocket;
@@ -97,41 +109,51 @@ function connectToServer () {
 	var host = window.location.host;
     var connection = new WebSocket('ws://'+host+':8081');
 
+    //When a connection opens
     connection.onopen = function () {
         // connection is opened and ready to use
+
+        //Reset reconnection counter
         retries = 0;
+
 		setConnectionStatus("Connected");
 		
+		//Read screen name from URL
         var name = getQueryVariable('name');
         if(typeof name == "undefined")
         	name = "default";
         console.log("Name: " + name);
         
-        
+        //Send a connection message to the server
 		connection.send('Connect: ' + name);
     };
 
+    //When something goes wrong
     connection.onerror = function (error) {
     	if(typeof error.data != "undefined")
         	console.error("Error data: " + error.data);
     };
     
+    //When a connection closes
     connection.onclose = function(close) {
     	console.log("The connection closed.");
 		setConnectionStatus("Disconnected");
     	reconnect();
     }
 
+    //When a message arrives from the server
     connection.onmessage = function (message) {
         // try to decode json (I assume that each messagse from server is json)
         console.debug("Got message");
         try {
+        	//Extract data from JSON string
             var json = JSON.parse(message.data);
 			currentInformation = json;
 			mainContentCounter = 0;
 			
 			console.debug("Feed name: " + json.name);
 			
+			//Force immediate change if panic feed
 			if(!running || json.name == "panic"){
 				clearTimeout(mainContentSwitchingTimeout);
 				switchMainInformation();
@@ -144,12 +166,15 @@ function connectToServer () {
     };
 };
 
+//Reconnection loop
 function reconnect() {
 	var cooldown = 1000;
 	if(retries > 5) {
 		cooldown *= 30;
 	}
 	
+	//Gives up after 10 attempts to reconnect
+	//TODO: Change this to infinite attempts?
 	if(retries < 10) {
 		console.log("Will reconnect in " + (cooldown/1000) + "s.");
 		setTimeout(connectToServer, cooldown);
@@ -157,10 +182,12 @@ function reconnect() {
 	}
 }
 
+//Display connection status on the screen
 function setConnectionStatus(statusText) {
 	document.getElementById("topBarRight").innerHTML = statusText;
 }
 
+//Get variables from the URL
 function getQueryVariable(variable) {
 	var query = window.location.search.substring(1);
 	var vars = query.split("&");
