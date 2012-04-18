1647ed23f0dec8eba09cf2f661282273863f4679
Erik Hedvall <erikwhedvall@gmail.com>
2012-04-18 21:07:19 +0200

    Fixed the service API. It is useable now.

--- services/Api.php
+++ services/Api.php
@@ -8,11 +8,21 @@
  * getParameters
  * Prints a JSON string with all the required parameters, containing key, label
  * and default value.
+ *
+ * Additional parameters:
+ * 		service - The name of the service. 
+ *
+ * Query example: Api.php?getParameters&service=RSS
  * Output syntax: {key1:{"label":labelText, "value":defaultValue}, key2:...}
 **/
-if(isset($_GET["getParameters"])) {
-	$service = new $SERVICE_NAME();
-	echo $service->getParameters();
+if(isset($_GET["getParameters"]) && isset($_GET["service"])) {
+	$directory = $_GET["service"];
+
+	if(file_exists($directory)){
+		include_once("$directory/service.php");
+		$service = new $SERVICE_NAME();
+		echo $service->getParameters();
+	}
 }
 
 ?>
