5167922ef2215f501127e06cffa4817680724bda
Unn Swanstrom <unnswanstrom@hotmail.com>
2012-05-11 16:27:46 +0200

    har snyggat till klienten

--- coffeescreen/client/frontend.js
+++ coffeescreen/client/frontend.js
@@ -78,7 +78,7 @@ function mainPostLoaded() {
 		totalTime *= document.getElementById("mainContent").offsetHeight;
 	}
 	else{
-		//Setup progress bra variables
+		//Setup progress bar variables
 		totalTime = displaytime;
 	}
 
