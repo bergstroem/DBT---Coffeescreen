c7ec8fbb147a57f1b79faddc7d77494eaed949ae
ser0n <andre.lilja@gmail.com>
2012-05-18 13:34:38 +0200

    Powerusage test

--- services/Powerusage/service.php
+++ services/Powerusage/service.php
@@ -8,7 +8,10 @@ class PowerUsage extends Service {
 	 * createParameter.
 	**/
 	protected function specifyParameters() {
-		$this->createParameter("power", "XML path", "Path to the xml with powerdata", Type::ShortText);
+		//$this->createParameter("power", "XML path", "Path to the xml with powerdata", Type::ShortText);
+		$this->createParameter("latest", "Only latest event",
+				"Check this if you only want to display the latest historical event of today.",
+				Type::Boolean);
 	}
 
 	/**
@@ -16,7 +19,7 @@ class PowerUsage extends Service {
 	 * parameters. These should be bundeled using bundleView and returned.
 	**/
 	public function getViews() {
-		$path = "";
+		/*$path = "";
 		$xml = simplexml_load_file("http://titan.codemill.se/~denols/infoboard/power/graphs/lastdata.xml");
 	$arr = array();
 	foreach($xml->data->row as $row){
@@ -36,7 +39,29 @@ class PowerUsage extends Service {
 		
 		$title = "Power usage";
 		
-		$this->bundleView($title, time(), $html);
+		$this->bundleView($title, time(), $html);*/
+		$day = date("j");
+		$month = date("F");
+
+		$onlyLatest = $this->readParameter("latest");
+
+		//Load the wikipedia data and pick out the event list
+		$html = file_get_html("http://en.wikipedia.org/wiki/".$month."_".$day);
+		$events = $html->find("div[id=mw-content-text] ul", 1)->find("li");
+
+		//Select a random event and display it
+		$content = "<h1>Today in History</h1>";
+		if($onlyLatest)
+			$content .= "<p>".$events[count($events)-1]->plaintext."</p>";
+		else
+			$content .= "<p>".$events[rand(0, count($events)-1)]->plaintext."</p>";
+
+		$content .= "<p style=\"font-size: 80%;\">";
+		$content .= "Source: http://en.wikipedia.org/wiki/".$month."_".$day;
+		$content .= "</p>";
+
+		$title = "Today in History";
+		$this->bundleView($title, time(), $content);
 	}
 }
 
