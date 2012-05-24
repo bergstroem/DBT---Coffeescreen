7ac18dd12db9c5919720fcf7470c9d4ffe60a2de
ser0n <andre.lilja@gmail.com>
2012-05-24 14:04:59 +0200

    test

--- plugins/Powerusage/power.js
+++ plugins/Powerusage/power.js
@@ -1,29 +1,12 @@
-console.log("haihai");
-google.load('visualization', '1.0', {packages:['corechart']});
-google.setOnLoadCallback(drawChart);
+$(document).ready(function(){
+	console.log("haihai");
+	google.load('visualization', '1.0', {packages:['corechart']});
+	google.setOnLoadCallback(drawChart);	
+});
 
 function drawChart() {
-	console.log("baibai");
-	var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
-
-        // Set chart options
-        var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':400,
-                       'height':300};
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-	/*var data = google.visualization.arrayToDataTable([
+	console.log("derp");
+	var data = google.visualization.arrayToDataTable([
 		['Label', 'Value'],
 		['Memory', 80],
 		['CPU', 55],
@@ -38,7 +21,7 @@ function drawChart() {
 	};
 
 	var chart = new google.visualization.Gauge(document.getElementById('disp'));
-	chart.draw(data, options);*/
+	chart.draw(data, options);
 }
 /*
 $.ajax({
