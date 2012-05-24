dba2a44b5b0aafec4d5b6e6c6084afc4d7dc75b8
ser0n <andre.lilja@gmail.com>
2012-05-24 12:20:06 +0200

    minor fix

--- plugins/Powerusage/power.js
+++ plugins/Powerusage/power.js
@@ -1,5 +1,27 @@
 var disp = document.getElementById("disp");
 
+google.load('visualization', '1', {packages:['gauge']});
+google.setOnLoadCallback(drawChart);
+
+function drawChart() {
+	var data = google.visualization.arrayToDataTable([
+		['Label', 'Value'],
+		['Memory', 80],
+		['CPU', 55],
+		['Network', 68]
+	]);
+
+	var options = {
+		width: 400, height: 120,
+		redFrom: 90, redTo: 100,
+		yellowFrom:75, yellowTo: 90,
+		minorTicks: 5	
+	};
+
+	var chart = new google.visualization.Gauge(disp);
+	chart.draw(data, options);
+}
+
 $.ajax({
 	type: 'POST',
 	url: '../plugins/Powerusage/powerusage.php',
