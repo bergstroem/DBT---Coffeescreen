582ed4dd292b1be044d892bb4e11fd1c173d1805
ser0n <andre.lilja@gmail.com>
2012-05-24 15:22:38 +0200

    update

--- plugins/Powerusage/plugin.php
+++ plugins/Powerusage/plugin.php
@@ -16,7 +16,7 @@ class Powerusage extends Plugin {
 	**/
 	public function getViews() {
 		$time = $this->readParameter("time");
-		$html = '<div id="powertext"><canvas id="gauge"></canvas></div>';
+		$html = '<canvas id="gauge"></canvas><div id="powertext"></div>';
 		$js = "../plugins/Powerusage/power.js";
 		$css = "../plugins/Powerusage/power.css";
 		
