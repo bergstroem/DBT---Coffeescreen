290df583f6ed821a86224c0b577ef11753e5346f
ser0n <andre.lilja@gmail.com>
2012-05-24 14:54:57 +0200

    test

--- plugins/Powerusage/power.js
+++ plugins/Powerusage/power.js
@@ -3,20 +3,61 @@ $.ajax({
 	url: '../plugins/Powerusage/powerusage.php',
 	data: '',
 	success: function(msg){
-		var val = parseFloat(msg);
-		var rotval = (val/9000+1)%2;
-		console.log(rotval);
+		var power = parseFloat(msg);
 		
-		document.getElementById('disp').appendChild(document.createTextNode(val + 'W'))
-
-		var c = document.getElementById("gauge");
-		var ctx = c.getContext("2d");
-		ctx.beginPath();
-		ctx.arc(150,150,100,1*Math.PI,rotval*Math.PI);
-		ctx.lineWidth = 15;
-		ctx.strokeStyle = "white";
-		ctx.stroke();
+		draw(power);
+		document.getElementById('disp').appendChild(document.createTextNode(power + 'W'))
 	}
 });
 
 
+function draw(power){
+	var rotval = (val/9000+1)%2;
+	var c = document.getElementById("gauge");
+	var ctx = c.getContext("2d");
+	if(power > 3000){
+		/* Green part */
+		ctx.beginPath();
+		ctx.arc(150,150,100,1*Math.PI,1.3*Math.PI);
+		ctx.lineWidth = 15;
+		ctx.strokeStyle = "green";
+		ctx.stroke();
+		
+		/* Yellow part */
+		ctx.beginPath();
+		ctx.arc(150,150,100,1.3*Math.PI,1.5*Math.PI);
+		ctx.lineWidth = 15;
+		ctx.strokeStyle = "yellow";
+		ctx.stroke();
+		
+		/* Red part */
+		ctx.beginPath();
+		ctx.arc(150,150,100,1.5*Math.PI,rotval*Math.PI);
+		ctx.lineWidth = 15;
+		ctx.strokeStyle = "red";
+		ctx.stroke();
+	}
+	else if(power > 2000){
+		/* Green part */
+		ctx.beginPath();
+		ctx.arc(150,150,100,1*Math.PI,1.3*Math.PI);
+		ctx.lineWidth = 15;
+		ctx.strokeStyle = "green";
+		ctx.stroke();
+		
+		/* Yellow part */
+		ctx.beginPath();
+		ctx.arc(150,150,100,1.3*Math.PI,rotval*Math.PI);
+		ctx.lineWidth = 15;
+		ctx.strokeStyle = "yellow";
+		ctx.stroke();
+	}
+	else{
+		/* Green part */
+		ctx.beginPath();
+		ctx.arc(150,150,100,1*Math.PI,rotval*Math.PI);
+		ctx.lineWidth = 15;
+		ctx.strokeStyle = "green";
+		ctx.stroke();
+	}
+}
