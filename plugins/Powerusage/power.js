e232292f47810dc80aa3f4f96fc76ba8d8b1aabb
ser0n <andre.lilja@gmail.com>
2012-05-24 11:23:45 +0200

    powerplugin

--- plugins/Powerusage/power.js
+++ plugins/Powerusage/power.js
@@ -10,22 +10,23 @@ $.ajax({
 		var disp = document.getElementById("disp");
 		google.load('visualization', '1', {packages:['gauge']});
 		google.setOnLoadCallBack(drawChart);
+		function drawChart(){
+			var data = google.visualization.arrayToDataTable([
+				['Label', 'Value'],
+				['Power', 80]
+			]);
+			var options = {
+				width: 400, height: 120,
+				redFrom: 90, redTo: 100,
+				yellowFrom:75, yellowTo:90,
+				minorTicks:5
+			};
+			
+			var chart = new google.visualization.Gauge(document.getElementById('disp'));
+			chart.draw(data, options);
+		}
 		//document.getElementById('disp').appendChild(document.createTextNode(val + 'W'))
 	}
 });
 
-function drawChart(){
-	var data = google.visualization.arrayToDataTable([
-		['Label', 'Value'],
-		['Power', 80]
-	]);
-	var options = {
-		width: 400, height: 120,
-		redFrom: 90, redTo: 100,
-		yellowFrom:75, yellowTo:90,
-		minorTicks:5
-	};
-	
-	var chart = new google.visualization.Gauge(document.getElementById('disp'));
-	chart.draw(data, options);
-}
+
