79242788d131fdb4130362e64654c203214cea7c
ser0n <andre.lilja@gmail.com>
2012-05-24 15:00:32 +0200

    added background line

--- plugins/Powerusage/power.js
+++ plugins/Powerusage/power.js
@@ -15,39 +15,46 @@ function draw(power){
 	var rotval = (power/9000+1)%2;
 	var c = document.getElementById("gauge");
 	var ctx = c.getContext("2d");
-	if(power > 3000){
+	
+	ctx.beginPath();
+	ctx.arc(150,150,100,1*Math.PI,0*Math.PI);
+	ctx.lineWidth = 15;
+	ctx.strokeStyle = "gray";
+	ctx.stroke();
+	
+	if(power > 7200){
 		/* Green part */
 		ctx.beginPath();
-		ctx.arc(150,150,100,1*Math.PI,1.3*Math.PI);
+		ctx.arc(150,150,100,1*Math.PI,1.5*Math.PI);
 		ctx.lineWidth = 15;
 		ctx.strokeStyle = "green";
 		ctx.stroke();
 		
 		/* Yellow part */
 		ctx.beginPath();
-		ctx.arc(150,150,100,1.3*Math.PI,1.5*Math.PI);
+		ctx.arc(150,150,100,1.5*Math.PI,1.8*Math.PI);
 		ctx.lineWidth = 15;
 		ctx.strokeStyle = "yellow";
 		ctx.stroke();
 		
 		/* Red part */
 		ctx.beginPath();
-		ctx.arc(150,150,100,1.5*Math.PI,rotval*Math.PI);
+		ctx.arc(150,150,100,1.8*Math.PI,rotval*Math.PI);
 		ctx.lineWidth = 15;
 		ctx.strokeStyle = "red";
 		ctx.stroke();
 	}
-	else if(power > 2000){
+	else if(power > 4500){
 		/* Green part */
 		ctx.beginPath();
-		ctx.arc(150,150,100,1*Math.PI,1.3*Math.PI);
+		ctx.arc(150,150,100,1*Math.PI,1.5*Math.PI);
 		ctx.lineWidth = 15;
 		ctx.strokeStyle = "green";
 		ctx.stroke();
 		
 		/* Yellow part */
 		ctx.beginPath();
-		ctx.arc(150,150,100,1.3*Math.PI,rotval*Math.PI);
+		ctx.arc(150,150,100,1.5*Math.PI,rotval*Math.PI);
 		ctx.lineWidth = 15;
 		ctx.strokeStyle = "yellow";
 		ctx.stroke();
