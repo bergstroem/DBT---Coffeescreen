6e16d81c0ff319b4a85f4dce39d7c2d9440c1d21
ser0n <andre.lilja@gmail.com>
2012-05-24 12:13:11 +0200

    patch

--- plugins/Powerusage/power.js
+++ plugins/Powerusage/power.js
@@ -6,24 +6,7 @@ $.ajax({
 	data: '',
 	success: function(msg){
 		var val = parseFloat(msg);
-		/*
-		google.load('visualization', '1', {packages:['gauge']});
-		google.setOnLoadCallBack(drawChart);
-		function drawChart(){
-			var data = google.visualization.arrayToDataTable([
-				['Label', 'Value'],
-				['Power', 80]
-			]);
-			var options = {
-				width: 400, height: 120,
-				redFrom: 90, redTo: 100,
-				yellowFrom:75, yellowTo:90,
-				minorTicks:5
-			};
-			
-			var chart = new google.visualization.Gauge(disp);
-			chart.draw(data, options);
-		}*/
+		
 		disp.appendChild(document.createTextNode(val + 'W'))
 	}
 });
