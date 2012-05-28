9508359722c23e57f26281172026fd8fd2f44ae1
ser0n <andre.lilja@gmail.com>
2012-05-28 14:18:52 +0200

    Fixed watermark include

--- admin/adminpage/channel.php
+++ admin/adminpage/channel.php
@@ -7,7 +7,7 @@
 		<script type="text/javascript" src="js/jquery-1.7.1.js"></script>
 		<script type="text/javascript" src="js/drag.js"></script>
 		<script type="text/javascript" src="js/channel.js"></script>
-		<script type="text/javascript" src="js/jquery.watermark.js"></script>
+		<script type="text/javascript" src="js/jquery.watermark.min.js"></script>
 		<meta charset="UTF-8"/>
 	</head>
 	<body>
